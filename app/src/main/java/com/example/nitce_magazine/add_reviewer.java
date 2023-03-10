45c09087e1a48257a4fa4c6afe6055cd1fd89db0
Shivam Kr. Jha <shivamkumarjhadbg6@gmail.com>
2023-03-10 14:29:19 +0530

    addReviewer page xml

---
+++ app/src/main/java/com/example/nitce_magazine/add_reviewer.java
@@ -0,0 +1,14 @@
+package com.example.nitce_magazine;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class add_reviewer extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_reviewer);
+    }
+}
